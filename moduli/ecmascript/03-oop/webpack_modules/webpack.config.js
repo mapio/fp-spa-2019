65ee6d525e8ad561bd3f59dcfc143cf0265de4d6
Massimo Santini <massimo.santini@gmail.com>
2019-06-11 16:51:25 +0200

    Fix path

--- moduli/ecmascript/03-oop/webpack_modules/webpack.config.js
+++ moduli/ecmascript/03-oop/webpack_modules/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: true,
             template: './src/index.html',
-            filename: './dist/index.html',
+            filename: './index.html',
             hash: true
         }),
     ],
